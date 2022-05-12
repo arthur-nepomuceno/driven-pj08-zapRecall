6d2129359637fa25ff16c3491d0a05c73c115536
arthur nepomuceno <arth.nepomuceno@gmail.com>
2022-05-12 06:54:32 -0300

    [DRIVEN][PJ08][ZapRecall]-0.1-welcome page

---
+++ src/containers/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Welcome(){
+
+    return (        
+        <div className="welcome">
+            <img src="assets/img/logo.png" alt="zap recall lightning"/>
+            <p>ZapRecall</p>
+            <button>Iniciar Recall!</button>
+        </div>      
+        
+    );
+}
