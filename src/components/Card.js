af57659dcc15416a6c85b96f2c92a2258f84af7e
arthur nepomuceno <arth.nepomuceno@gmail.com>
2022-05-12 15:35:51 -0300

    [DRIVEN][PJ08][ZapRecall]-0.4-start page/card answer buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,44 @@
+import React from 'react';
+
 export default function Card(){
+    
+    const buttons = [{bgcolor:"background-red", text:"Não lembrei",
+                    cardStyle: "red-text cross-text", icon:"close-circle", colorID:"red"},
+
+                    {bgcolor:"background-yellow", text:"Quase não lembrei",
+                    cardStyle: "yellow-text cross-text", icon:"help-circle", colorID:"yellow"},
+
+                    {bgcolor:"background-green", text:"Zap!",
+                    cardStyle: "green-text cross-text", icon:"checkmark-circle", colorID:"green"}]
+
+    const [classStyle, setClassStyle] = React.useState('black-text');
+    const [iconName, setIconName] = React.useState('play-outline');
+    const [iconColor, setIconColor] = React.useState('black');
+
+    function markAnswer(classStyle, iconName, iconColor){
+        setClassStyle(classStyle);
+        setIconName(iconName);
+        setIconColor(iconColor);
+    }
+
     return(
         <div className="card">
             <div className="cover">
-                <p>Pergunta 1</p>
-                <ion-icon name="play-outline"></ion-icon>
+                <p className={classStyle}>Pergunta 1</p>
+                <ion-icon id={iconColor} name={iconName}></ion-icon>
             </div>
             <div className="question">
                 <p>O que é JSX?</p>
                 <ion-icon name="repeat-outline"></ion-icon>
+            </div>
+            <div className="answer">
+                <p>Uma extensão de linguagem do JavaScript</p>
+                {buttons.map((button, index) => <button
+                                                key={index}
+                                                className={button.bgcolor} 
+                                                onClick={() => {markAnswer(button.cardStyle, 
+                                                                            button.icon, 
+                                                                            button.colorID)}}>{button.text}</button>)}                
             </div>
         </div>
     );
